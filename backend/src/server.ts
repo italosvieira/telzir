b1037b428f749c8917a2c0c21db381f4fad40123
italosvieira <italo.vieira.dev@gmail.com>
2019-08-17 12:58:28 -0300

    Commit inicial do backend.

---
+++ backend/src/server.ts
@@ -0,0 +1,9 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function start() {
+    const server = await NestFactory.create(AppModule);
+    await server.listen(3000);
+}
+
+start();
